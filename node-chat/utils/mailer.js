9b7f4f93be0d325ea54e51ec04d93aad57733916
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 22:23:19 +0300

    bbb

--- node-chat/utils/mailer.js
+++ node-chat/utils/mailer.js
@@ -2,12 +2,12 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
   secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: "00fa0e1c792a00",
+    pass: e72ac6aadab258,
   },
 });
 const sendEmail = async (to, code) => {
