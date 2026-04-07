40962f780412816b278f4e9f3dcbebb5fd60df6a
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 23:06:23 +0300

    Update middleware.jsj

--- chat-app/app/middleware.js
+++ chat-app/app/middleware.js
@@ -1,22 +1,23 @@
-
 import { NextResponse } from "next/server";
+
 export function middleware(request) {
-  const token = request.cookies.get("userToken").value;
-  console.log(token,"token")
-  const isProtectedRout =
-    request.nextUrl.pathname.startsWith("/Profile") ||
-    request.nextUrl.pathname.startsWith("/dashboard");
- 
-  if (isProtectedRout && !token) {
+  const token = request.cookies.get("userToken")?.value;
+  const { pathname } = request.nextUrl;
+
+  // 1. إذا كان المستخدم يملك توكن ويحاول دخول صفحة تسجيل الدخول، انقله للداشبورد
+  if (token && (pathname === "/login"  pathname === "/verifyCode"  pathname === "/")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // 2. إذا كان المستخدم لا يملك توكن ويحاول دخول الداشبورد أو الملف الشخصي
+  const isProtectedRoute = pathname.startsWith("/dashboard") || pathname.startsWith("/Profile");
+  if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
- 
+
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/Profile/:path*",
- 
-  ],
+  matcher: ["/dashboard/:path*", "/Profile/:path*", "/login", "/verifyCode", "/"],
 };
