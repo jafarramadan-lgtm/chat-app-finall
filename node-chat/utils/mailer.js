ee80400e01188cab401d913ac9bb6cba1c711538
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 21:41:14 +0300

    Update mailer.js

--- node-chat/utils/mailer.js
+++ node-chat/utils/mailer.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
   auth: {
     user: process.env.SMTP_USER,
