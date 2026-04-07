42cb2815274ed62be891ae44e6fd99b30851aeaa
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 22:48:50 +0300

    Update middleware.jsg

--- chat-app/app/middleware.js
+++ chat-app/app/middleware.js
@@ -5,24 +5,17 @@ export function middleware(request) {
   const isProtectedRout =
     request.nextUrl.pathname.startsWith("/Profile") ||
     request.nextUrl.pathname.startsWith("/dashboard");
-  const isAuthRoute =
-    request.nextUrl.pathname.startsWith("/login") ||
-    request.nextUrl.pathname.startsWith("/register") ||
-    request.nextUrl.pathname.startsWith("/verifyCode");
+ 
   if (isProtectedRout && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  if (isAuthRoute && token) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+ 
   return NextResponse.next();
 }
 export const config = {
   matcher: [
     "/dashboard/:path*",
     "/Profile/:path*",
-    "/login/:path*",
-    "/register/:path*",
-    "/verifyCode/:path*",
+ 
   ],
 };
