0b7a0789ea927229170b56ed3b788222e44d2c2e
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 22:52:55 +0300

    Update middleware.js

--- chat-app/app/middleware.js
+++ chat-app/app/middleware.js
@@ -1,1 +1,22 @@
 
+import { NextResponse } from "next/server";
+export function middleware(request) {
+  const token = request.cookies.get("userToken").value;
+  console.log(token,"token")
+  const isProtectedRout =
+    request.nextUrl.pathname.startsWith("/Profile") ||
+    request.nextUrl.pathname.startsWith("/dashboard");
+ 
+  if (isProtectedRout && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+ 
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/Profile/:path*",
+ 
+  ],
+};
