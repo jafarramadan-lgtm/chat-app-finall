f20fd7648d22c50b90b58f050771c9f17abe94a2
Jafar ramadan <ramadangafer5@gmail.com>
2026-04-07 21:19:41 +0300

    Update index.js22

--- node-chat/index.js
+++ node-chat/index.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://chat-app-finall-3s2u.vercel.app", //url front
+    origin: ["https://chat-app-finall-3s2u.vercel.app"], //url front
     credentials: true,
   }),
 );
